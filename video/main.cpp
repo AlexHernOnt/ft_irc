3d125147863219474320b8a151ee777b9fd7c55f
Alejandro Hernandez ontoria <ahernand@c1r8s6.42madrid.com>
2023-04-23 20:08:24 +0200

    now takes more than 1 client and all of thom send messages back, good progress, not enough

--- video/main.cpp
+++ video/main.cpp
@@ -6,37 +6,111 @@
 /*   By: ahernand <ahernand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 14:59:00 by ahernand          #+#    #+#             */
-/*   Updated: 2023/04/16 17:37:28 by ahernand         ###   ########.fr       */
+/*   Updated: 2023/04/23 20:07:05 by ahernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+//Example code: A simple server side code, which echos back the received message.
+//Handle multiple socket connections with select and fd_set on Linux
+
+#include <stdio.h> 
+#include <string.h>			//strlen 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <unistd.h>			//close 
+#include <arpa/inet.h>		//close 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/time.h>		//FD_SET, FD_ISSET, FD_ZERO macros 
 #include <iostream>
-#include <fstream> //probablemente va fuera
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 
-// Server side C/C++ program to demonstrate Socket
-// programming
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#include <sys/poll.h>
-
-#define PORT 8080
-
-int main(int argc, char const* argv[])
-{
-	int						server_fd, new_socket, valread;
+#define TRUE	1
+#define FALSE	0
+#define PORT	8888 
+	 
+int main(int argc , char *argv[])  
+{  
+	int						opt = TRUE;
+	int						master_socket;
+	int						addrlen;
+	int						new_socket;
+	int						client_socket[30];
+	int						max_clients = 30;
+	int						activity;
+	int						i;
+	int						valread;
+	int						sd;
+	int						max_sd;
 	struct sockaddr_in		address;
-	int						opt = 1;
-	int						addrlen = sizeof(address);
-	char					buffer[1024];
-	struct pollfd			pfds[1024];
-	int						numOfConnections = 0;
-	const char*				hello = "Server: Hello!\n";
+
+	char					buffer[1025];  //data buffer of 1K 
+	char					*message = "Server: Welcome client!\n";  
+
+
+
+
+
+
+
+
+
+
+
+
+	//set of socket descriptors 
+	fd_set readfds;  
+
+	//initialise all client_socket[] to 0 so not checked 
+	for (i = 0; i < max_clients; i++)  
+	{  
+		client_socket[i] = 0;  
+	}
+
+	//create a master socket 
+	if ((master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
+	{  
+		perror("socket failed");  
+		exit(EXIT_FAILURE);  
+	}  
+
+	//set master socket to allow multiple connections , 
+	//this is just a good habit, it will work without this 
+	if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
+		sizeof(opt)) < 0 )
+	{
+		perror("setsockopt");  
+		exit(EXIT_FAILURE);  
+	}
+
+	//type of socket created 
+	address.sin_family = AF_INET;  
+	address.sin_addr.s_addr = INADDR_ANY;  
+	address.sin_port = htons(PORT);  
+
+	//bind the socket to localhost port 8888 
+	if (bind(master_socket, (struct sockaddr *)&address, sizeof(address)) < 0)  
+	{  
+		perror("bind failed");  
+		exit(EXIT_FAILURE);  
+	}  
+	printf("Listener on port %d \n", PORT);  
+
+	//try to specify maximum of 3 pending connections for the master socket 
+	if (listen(master_socket, 3) < 0)  
+	{  
+		perror("listen");  
+		exit(EXIT_FAILURE);  
+	}  
+
+
+
+
+
+
+
 
 
 
@@ -46,126 +120,388 @@ int main(int argc, char const* argv[])
 
 
 	/*
-	**	Setting Up Server
+	**		Incorporar Cliente
 	*/
 
+	//accept the incoming connection 
+	addrlen = sizeof(address);  
+	puts("Waiting for connections ...");  
 
-	// Creating socket
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+	while(TRUE)  
+	{  
+		//clear the socket set
+		FD_ZERO(&readfds);  
+	 
+		//add master socket to set 
+		FD_SET(master_socket, &readfds);  
+		max_sd = master_socket;  
+			 
+		//add child sockets to set 
+		for ( i = 0 ; i < max_clients ; i++)  
+		{  
+			//socket descriptor 
+			sd = client_socket[i];  
+				 
+			//if valid socket descriptor then add to read list 
+			if(sd > 0)  
+				FD_SET( sd , &readfds);  
+				 
+			//highest file descriptor number, need it for the select function 
+			if(sd > max_sd)  
+				max_sd = sd;  
+		}  
 
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
+		//wait for an activity on one of the sockets , timeout is NULL , 
+		//so wait indefinitely 
+		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);  
+	   
+		if ((activity < 0) && (errno!=EINTR))  
+		{  
+			printf("select error");  
+		}  
+			 
+		//If something happened on the master socket , 
+		//then its an incoming connection 
+		if (FD_ISSET(master_socket, &readfds))  
+		{  
+			if ((new_socket = accept(master_socket, 
+					(struct sockaddr *)&address, (socklen_t*)&addrlen))<0)  
+			{  
+				perror("accept");  
+				exit(EXIT_FAILURE);  
+			}  
 
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
-  
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
+			//inform user of socket number - used in send and receive commands 
+			printf("New connection , socket fd is %d , ip is : %s , port : %d\n", new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
 
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-
-
-
-
-
-
-
-
-	/*
-	**	Setting Up Pull
-	*/
-
-
-	for(int i = 0; i < 1024; i++)
-	{
-		pfds[i].fd = -1;
-		pfds[i].events = POLLIN;
-	}
-
-
-	std::fill_n(buffer, 1024, 0);
-
-
-
-
-
-
-
-
-	/*
-	**	Server start
-	*/
-
-
-	while (42)
-	{
-		// Mirar si hay nueva conexion
-		new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-
-		if (new_socket != -1)
-		{
-			for (int i = 0; i < 1024; i++)
+			//send new connection greeting message
+			if (send(new_socket, message, strlen(message), 0) != strlen(message))
 			{
-				if (pfds[i].fd == new_socket)
-					break;
-				if (pfds[i].fd == -1)
-				{
-					pfds[i].fd = new_socket;
-					write(1, "New connection\n", 15);
-					send(new_socket, hello, strlen(hello), 0);
-					numOfConnections++;
-					break;
+				perror("send");
+			}
+
+			puts("Welcome message sent successfully");
+
+			//add new socket to array of sockets
+			for (i = 0; i < max_clients; i++)
+			{
+				//if position is empty 
+				if( client_socket[i] == 0 )  
+				{  
+					client_socket[i] = new_socket;  
+					printf("Adding to list of sockets as %d\n" , i);  
+
+					break;  
 				}
 			}
 		}
-		poll(pfds, numOfConnections, -1);
-		//comprobar todas las conexiones
-		for (int i = 0; i < numOfConnections; i++)
-        {
-			if (pfds[i].fd == -1)
-				break;
-			if (pfds[i].revents & POLLIN)
-			{
-				valread = read(pfds[i].fd, buffer, 1024);
-				if (valread > 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		/*
+		**		Disconnect and read
+		*/
+
+		//else its some IO operation on some other socket
+		for (i = 0; i < max_clients; i++)  
+		{  
+			sd = client_socket[i];  
+				 
+			if (FD_ISSET( sd , &readfds))  
+			{  
+				//Check if it was for closing , and also read the 
+				//incoming message 
+				if ((valread = read( sd , buffer, 1024)) == 0)  
 				{
-					write(1, buffer, valread);
-					std::fill_n(buffer, 1024, 0);
+					//Somebody disconnected , get his details and print
+					getpeername(sd , (struct sockaddr*)&address , \
+						(socklen_t*)&addrlen);
+					printf("Host disconnected , ip %s , port %d \n" ,
+						  inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+					//Close the socket and mark as 0 in list for reuse
+					close( sd );
+					client_socket[i] = 0;
 				}
+
+				//Echo back the message that came in
 				else
 				{
-					write(1, "stdin closed\n", 13);
+					//set the string  terminating NULL byte on the end
+					//of the data read
+				
+					buffer[valread] = '\0';
+					printf("%s", buffer);
+					std::fill_n(buffer, 1024, 0);
+				
+					// send(sd , buffer , strlen(buffer) , 0 );
 				}
 			}
 		}
 	}
 
-	send(new_socket, hello, strlen(hello), 0);
-	printf("Message sent\n");
-
-	// closing the connected socket
-	close(new_socket);
-	// closing the listening socket
-	shutdown(server_fd, SHUT_RDWR);
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Verson multi cliente beñat
+
+
+// #include <iostream>
+// #include <fstream> //probablemente va fuera
+// #include <sys/socket.h>
+// #include <netinet/in.h>
+// #include <unistd.h>
+
+// // Server side C/C++ program to demonstrate Socket
+// // programming
+// #include <netinet/in.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// #include <sys/poll.h>
+
+// #define PORT 8080
+
+// int main(int argc, char const* argv[])
+// {
+// 	int						server_fd, new_socket, valread;
+// 	struct sockaddr_in		address;
+// 	int						opt = 1;
+// 	int						addrlen = sizeof(address);
+// 	char					buffer[1024];
+// 	struct pollfd			pfds[1024];
+// 	int						numOfConnections = 0;
+// 	const char*				hello = "Server: Hello!\n";
+
+
+
+
+
+
+
+
+// 	/*
+// 	**	Setting Up Server
+// 	*/
+
+
+// 	// Creating socket
+// 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+// 	{
+// 		perror("socket failed");
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	// Forcefully attaching socket to the port 8080
+// 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)))
+// 	{
+// 		perror("setsockopt");
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	address.sin_family = AF_INET;
+// 	address.sin_addr.s_addr = INADDR_ANY;
+// 	address.sin_port = htons(PORT);
+  
+// 	// Forcefully attaching socket to the port 8080
+// 	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+// 	{
+// 		perror("bind failed");
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	if (listen(server_fd, 3) < 0)
+// 	{
+// 		perror("listen");
+// 		exit(EXIT_FAILURE);
+// 	}
+
+
+
+
+
+
+
+
+// 	/*
+// 	**	Setting Up Pull
+// 	*/
+
+
+// 	for(int i = 0; i < 1024; i++)
+// 	{
+// 		pfds[i].fd = -1;
+// 		pfds[i].events = POLLIN;
+// 	}
+
+
+// 	std::fill_n(buffer, 1024, 0);
+
+
+
+
+
+
+
+
+// 	/*
+// 	**	Server start
+// 	*/
+
+
+// 	while (42)
+// 	{
+// 		// Mirar si hay nueva conexion
+// 		new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+
+// 		if (new_socket != -1)
+// 		{
+// 			for (int i = 0; i < 1024; i++)
+// 			{
+// 				if (pfds[i].fd == new_socket)
+// 					break;
+// 				if (pfds[i].fd == -1)
+// 				{
+// 					pfds[i].fd = new_socket;
+// 					write(1, "New connection\n", 15);
+// 					send(new_socket, hello, strlen(hello), 0);
+// 					numOfConnections++;
+// 					break;
+// 				}
+// 			}
+// 		}
+// 		poll(pfds, numOfConnections, -1);
+// 		//comprobar todas las conexiones
+// 		for (int i = 0; i < numOfConnections; i++)
+// 		{
+// 			if (pfds[i].fd == -1)
+// 				break;
+// 			if (pfds[i].revents & POLLIN)
+// 			{
+// 				valread = read(pfds[i].fd, buffer, 1024);
+// 				if (valread > 0)
+// 				{
+// 					write(1, buffer, valread);
+// 					std::fill_n(buffer, 1024, 0);
+// 				}
+// 				else
+// 				{
+// 					write(1, "stdin closed\n", 13);
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	send(new_socket, hello, strlen(hello), 0);
+// 	printf("Message sent\n");
+
+// 	// closing the connected socket
+// 	close(new_socket);
+// 	// closing the listening socket
+// 	shutdown(server_fd, SHUT_RDWR);
+// 	return 0;
+// }
 
 
 
